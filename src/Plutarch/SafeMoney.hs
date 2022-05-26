c31d917c1721ec3838df2abb79de2be65e94965a
Koz Ross <koz.ross@retro-freedom.nz>
2022-05-26 15:11:08 +1200

    Extra PDiscrete functions

--- src/Plutarch/SafeMoney.hs
+++ src/Plutarch/SafeMoney.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE PackageImports #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
@@ -7,9 +8,15 @@
 
 module Plutarch.SafeMoney (
     PDiscrete (..),
+    pvalueDiscrete,
+    pvalueDiscrete',
+    pdiscreteValue,
+    pdiscreteValue',
 ) where
 
 import Data.Bifunctor (first)
+import Data.Kind (Type)
+import Data.Tagged (Tagged (Tagged))
 import Generics.SOP (I (I))
 import Generics.SOP.TH (deriveGeneric)
 import Plutarch (
@@ -17,14 +24,36 @@ import Plutarch (
     PlutusType,
     S,
     Term,
+    pcon,
+    phoistAcyclic,
+    plam,
+    unTermCont,
+    (#),
+    (#$),
+    (:-->),
+ )
+import Plutarch.Api.V1 (PValue)
+import Plutarch.Api.V1.AssetClass (
+    PAssetClass,
+    passetClass,
+    passetClassValueOf,
+ )
+import "liqwid-plutarch-extra" Plutarch.Api.V1.Value (
+    passetClassValue,
+    psingletonValue,
  )
 import Plutarch.Bool (PEq, POrd)
 import Plutarch.Builtin (PAsData, PData, PIsData)
+import Plutarch.Extra.Applicative (ppure)
+import Plutarch.Extra.Comonad (pextract)
 import Plutarch.Extra.Tagged (PTagged)
+import Plutarch.Extra.TermCont (pletC, pmatchC)
 import Plutarch.Integer (PInteger)
+import Plutarch.Lift (pconstant)
 import Plutarch.Show (PShow)
 import Plutarch.TryFrom (PTryFrom (PTryFromExcess, ptryFrom'))
 import Plutarch.Unsafe (punsafeCoerce)
+import Plutus.V1.Ledger.Value (AssetClass (AssetClass))
 
 -- | @since 1.0.0
 newtype PDiscrete (tag :: k) (s :: S)
@@ -66,9 +95,71 @@ deriving via
         (PTryFrom a PInteger) => PTryFrom a (PDiscrete tag)
 
 -- | @since 1.0.0
-instance--     (PTryFrom PData (PAsData PInteger)) =>
-    PTryFrom PData (PAsData (PDiscrete tag)) where
+instance PTryFrom PData (PAsData (PDiscrete tag)) where
     type
         PTryFromExcess PData (PAsData (PDiscrete tag)) =
             PTryFromExcess PData (PAsData PInteger)
-    ptryFrom' d k = ptryFrom' @_ @(PAsData PInteger) d $ k . first punsafeCoerce
+    ptryFrom' d k =
+        ptryFrom' @_ @(PAsData PInteger) d $ k . first punsafeCoerce
+
+{- | Downcast a 'PValue' to a 'PDiscrete' unit, providing a witness of the 'PAssetClass'.
+     @since 0.3
+-}
+pvalueDiscrete ::
+    forall k.
+    forall (tag :: k) (s :: S).
+    Term s (PAssetClass :--> PValue :--> PDiscrete tag)
+pvalueDiscrete = phoistAcyclic $
+    plam $ \ac f -> pcon . PDiscrete $ ppure # (passetClassValueOf # f # ac)
+
+{- | Downcast a 'PValue' to a 'PDiscrete' unit, providing a witness of the 'AssetClass', which gets inlined. If you use this 'AssetClass' twice, prefer 'pvalueDiscrete'.
+     @since 0.3
+-}
+pvalueDiscrete' ::
+    forall k.
+    forall (tag :: k) (s :: S).
+    Tagged tag AssetClass ->
+    Term s (PValue :--> PDiscrete tag)
+pvalueDiscrete' (Tagged (AssetClass (cs, tn))) = phoistAcyclic $
+    plam $ \f ->
+        pcon . PDiscrete $
+            ppure # (passetClassValueOf # f #$ passetClass # pconstant cs # pconstant tn)
+
+{- | Get a 'PValue' from a 'PDiscrete', providing a witness of the 'AssetClass'.
+     __NOTE__: `pdiscreteValue` after `pvalueDiscrete` is not a round-trip.
+     It filters for a particular tag.
+     @since 0.3
+-}
+pdiscreteValue ::
+    forall k.
+    forall (tag :: k) (s :: S).
+    Term s (PAssetClass :--> PDiscrete tag :--> PValue)
+pdiscreteValue = phoistAcyclic $
+    plam $ \ac p -> unTermCont $ do
+        PDiscrete t <- pmatchC p
+        v <- pletC (pextract # t)
+        pure $ passetClassValue # ac # v
+
+{- | Get a 'PValue' from a 'PDiscrete', providing a witness of the 'AssetClass'.
+     __NOTE__: `pdiscreteValue` after `pvalueDiscrete` is not a round-trip.
+     It filters for a particular tag.
+     If you use this 'AssetClass' twice, prefer 'pvalueDiscrete'.
+     @since 0.3
+-}
+pdiscreteValue' ::
+    forall (tag :: Type) (s :: S).
+    Tagged tag AssetClass ->
+    Term s (PDiscrete tag :--> PValue)
+pdiscreteValue' (Tagged (AssetClass (cs, tn))) =
+    phoistAcyclic $
+        plam $ \p -> unTermCont $ do
+            PDiscrete t <- pmatchC p
+            pure $ psingletonValue # pconstant cs # pconstant tn # (pextract # t)
+
+{-
+pdiscreteValue' (Tagged (AssetClass (cs, tn))) = phoistAcyclic $
+  plam $ \p ->
+    psingletonValue
+      # pconstant cs
+      # pconstant tn
+      # Tagged.puntag p -}
