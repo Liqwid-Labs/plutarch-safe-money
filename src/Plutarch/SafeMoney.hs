b16308643f9d08a483666bf5a77a1c42f95b59a4
Koz Ross <koz.ross@retro-freedom.nz>
2022-05-26 13:59:15 +1200

    PTryFrom for PDiscrete

--- src/Plutarch/SafeMoney.hs
+++ src/Plutarch/SafeMoney.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE PolyKinds #-}
 
 module Plutarch.SafeMoney (
     PDiscrete (..),
 ) where
 
+import Data.Bifunctor (first)
 import Generics.SOP (I (I))
 import Generics.SOP.TH (deriveGeneric)
 import Plutarch (
@@ -17,10 +19,12 @@ import Plutarch (
     Term,
  )
 import Plutarch.Bool (PEq, POrd)
-import Plutarch.Builtin (PIsData)
+import Plutarch.Builtin (PAsData, PData, PIsData)
 import Plutarch.Extra.Tagged (PTagged)
 import Plutarch.Integer (PInteger)
 import Plutarch.Show (PShow)
+import Plutarch.TryFrom (PTryFrom (PTryFromExcess, ptryFrom'))
+import Plutarch.Unsafe (punsafeCoerce)
 
 -- | @since 1.0.0
 newtype PDiscrete (tag :: k) (s :: S)
@@ -54,3 +58,17 @@ deriving via
 
 -- | @since 1.0.0
 deriving anyclass instance PShow (PDiscrete tag)
+
+-- | @since 1.0.0
+deriving via
+    (DerivePNewtype (PDiscrete tag) (PTagged tag PInteger))
+    instance
+        (PTryFrom a PInteger) => PTryFrom a (PDiscrete tag)
+
+-- | @since 1.0.0
+instance--     (PTryFrom PData (PAsData PInteger)) =>
+    PTryFrom PData (PAsData (PDiscrete tag)) where
+    type
+        PTryFromExcess PData (PAsData (PDiscrete tag)) =
+            PTryFromExcess PData (PAsData PInteger)
+    ptryFrom' d k = ptryFrom' @_ @(PAsData PInteger) d $ k . first punsafeCoerce
