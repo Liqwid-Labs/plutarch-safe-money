cff367d3fd67c377cf9f4025a0e84f2350d7155a
Hongrui Fang <chfanghr@gmail.com>
2022-07-30 22:46:52 +0800

    use hydra ci
    Co-authored-by: SeungheonOh <seungheon.ooh@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.follows = "plutarch/nixpkgs";
     nixpkgs-latest.url = "github:NixOS/nixpkgs?rev=a0a69be4b5ee63f1b5e75887a406e9194012b492";
+
     # temporary fix for nix versions that have the transitive follows bug
     # see https://github.com/NixOS/nix/issues/6013
     nixpkgs-2111 = { url = "github:NixOS/nixpkgs/nixpkgs-21.11-darwin"; };
@@ -15,11 +16,7 @@
 
     # Plutarch and its friends
     plutarch = {
-      url =
-        "github:Liqwid-Labs/plutarch?ref=staging";
-
-      inputs.haskell-nix-extra-hackage.url =
-        "github:mlabs-haskell/haskell-nix-extra-hackage?ref=main";
+      url = "github:Liqwid-Labs/plutarch?ref=staging";
 
       inputs.emanote.follows =
         "plutarch/haskell-nix/nixpkgs-unstable";
@@ -48,6 +45,17 @@
           "${inputs.plutarch-numeric}"
           "${inputs.liqwid-plutarch-extra}"
         ])
+        (liqwid-nix.addChecks {
+          plutarch-safe-money = "plutarch-safe-money:lib:plutarch-safe-money";
+        })
+        (liqwid-nix.enableFormatCheck [
+          "-XTemplateHaskell"
+          "-XTypeApplications"
+          "-XPatternSynonyms"
+        ])
+        liqwid-nix.enableCabalFormatCheck
+        liqwid-nix.enableNixFormatCheck
+        liqwid-nix.enableLintCheck
       ]
     ).toFlake;
 }
